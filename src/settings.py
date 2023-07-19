c7b0b8cc31fda5d763dbb415d2c9bb9e9d0d9b01
Павел Смирнов <baksbannysmirnov@yandex.ru>
2023-07-19 18:08:25 +0200

    Create global project config

---
+++ src/settings.py
@@ -0,0 +1,41 @@
+import os
+import uuid
+from datetime import time
+from datetime import timedelta
+from functools import cache
+from pathlib import Path
+from urllib import urljoin
+
+from pydantic import BaseSettings
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+class Settings(BaseSettings):
+    """Global project settings"""
+
+    app_title: str = "RandomCoffeeBot"
+    description: str
+
+    class Config:
+        env_file = '.env'
+        env_file_encoding = 'utf-8'
+
+    # Settings connect to the database
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str = "random_coffee_db"
+    DB_HOST: str = "localhost"
+    DB_PORT: str = 5432
+
+    @property
+    def database_url(self) -> str:
+        """Get a link to connect to the database"""
+        return (
+            "postgresql+asyncpg://"
+            f"{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}"
+            f"@{self.DB_HOST}:{self.DB_PORT}/{self.POSTGRES_DB}"
+        )
+
+
+settings = Settings()
