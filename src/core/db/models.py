6aac02e030ad20d588b84be8050c126ce633ea6d
Павел Смирнов <baksbannysmirnov@yandex.ru>
2023-07-20 18:08:21 +0200

    Added fields id, created_at, updated_at

--- src/core/db/models.py
+++ src/core/db/models.py
@@ -1,7 +1,17 @@
-from sqlalchemy.orm import DeclarativeBase
+from datetime import date
+
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.sql import func
 
 
 class Base(DeclarativeBase):
     """Base class for models"""
 
-    pass
+    id: Mapped[ind] = mapped_column(primary_key=True)
+    created_at: Mapped[date] = mapped_column(
+        server_default=func.current_timestamp()
+    )
+    updated_at: Mapped[date] = mapped_column(
+        server_default=func.current_timestamp(),
+        onupdate=func.current_timestamp()
+    )
