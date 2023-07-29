33e36d6206be33e8d4f4d3ccb713ebe471c5f4e0
Dima Shelepin <oladushkin777@gmail.com>
2023-07-30 01:00:59 +0300

    Исправления AbstractRepository

--- src/core/db/repository/base.py
+++ src/core/db/repository/base.py
@@ -21,8 +21,9 @@ class AbstractRepository(abc.ABC, Generic[Model]):
 
     async def get_or_none(self, instance_id: int) -> Model | None:
         """Получает из базы объект модели по ID. В случае отсутствия возвращает None."""
-        instance = await self._sessionmaker.execute(select(self._model).where(self._model.id == instance_id))
-        return instance.scalars().first()
+        async with self._sessionmaker() as session:
+            instance = await session.scalar(select(self._model).where(self._model.id == instance_id))
+        return instance
 
     async def get(self, instance_id: int) -> Model:
         """Получает объект модели по ID. В случае отсутствия объекта бросает ошибку."""
@@ -33,33 +34,37 @@ class AbstractRepository(abc.ABC, Generic[Model]):
 
     async def create(self, instance: Model) -> Model:
         """Создает новый объект модели и сохраняет в базе."""
-        self._sessionmaker.add(instance)
-        try:
-            await self._sessionmaker.commit()
-        except IntegrityError:
-            raise exceptions.ObjectAlreadyExistsError(instance)
-
-        await self._sessionmaker.refresh(instance)
+        async with self._sessionmaker() as session:
+            session.add(instance)
+            try:
+                await session.commit()
+            except IntegrityError:
+                raise exceptions.ObjectAlreadyExistsError(instance)
+            await session.refresh(instance)
         return instance
 
     async def update(self, instance_id: int, instance: Model) -> Model:
         """Обновляет существующий объект модели в базе."""
-        instance.id = instance_id
-        instance = await self._sessionmaker.merge(instance)
-        await self._sessionmaker.commit()
-        return instance  # noqa: R504
+        async with self._sessionmaker() as session:
+            instance.id = instance_id
+            instance = await session.merge(instance)
+            await session.commit()
+        return instance
 
     async def update_all(self, instances: list[dict]) -> list[Model]:
         """Обновляет несколько измененных объектов модели в базе."""
-        update_objects = await self._sessionmaker.execute(update(self._model), instances)
-        await self._sessionmaker.commit()
-        return update_objects
+        async with self._sessionmaker() as session:
+            await session.execute(update(self._model), instances)
+        return instances
 
     async def get_all(self) -> list[Model]:
         """Возвращает все объекты модели из базы данных."""
-        objects = await self._sessionmaker.execute(select(self._model))
-        return objects.scalars().all()
+        async with self._sessionmaker() as session:
+            objects = await session.scalars(select(self._model))
+        return objects
 
-    async def create_all(self, objects: list[Model]) -> None:
+    async def create_all(self, instances: list[Model]) -> None:
         """Создает несколько объектов модели в базе данных."""
-        self._sessionmaker.add_all(objects)
+        async with self._sessionmaker() as session:
+            session.add_all(instances)
+            await session.commit()
