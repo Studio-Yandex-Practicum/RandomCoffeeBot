bffd189c8fce84fc8031542cbfa5c55bbba8d0af
Павел Смирнов <baksbannysmirnov@yandex.ru>
2023-07-29 16:59:17 +0200

    RandomCoffeeBoot-21: Get engine and async_sessionmaker from DI container

--- src/depends.py
+++ src/depends.py
@@ -1,7 +1,27 @@
 from dependency_injector import containers, providers
 
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine
+)
+
 from .settings import Settings
+
+DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost:5432/postgres"
 
 
 class Container(containers.DeclarativeContainer):
     settings = providers.Singleton(Settings)
+    engine = providers.Singleton(
+        create_async_engine,
+        DATABASE_URL,
+        future=True,
+        echo=True
+    )
+    async_session_maker = providers.Singleton(
+        async_sessionmaker,
+        engine,
+        expire_on_commmit=False
+    )
+    AsyncSessionMaker = async_sessionmaker[AsyncSession]
