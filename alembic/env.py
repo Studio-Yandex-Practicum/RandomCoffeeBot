69ea5a04c9ae8656192be144485b0604f1cb4071
tmbkv <101175488+tmbkv@users.noreply.github.com>
2023-08-03 17:12:58 +0300

    Update env.py
    Добавил класс сеттингс

--- alembic/env.py
+++ alembic/env.py
@@ -6,10 +6,11 @@ from sqlalchemy.ext.asyncio import AsyncEngine
 
 from alembic import context
 from src.core.db.models import Base
+from src.settings import Settings
 
 config = context.config
 
-config.set_main_option("sqlalchemy.url", settings.database_url)
+config.set_main_option("sqlalchemy.url", Settings.database_url)
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
