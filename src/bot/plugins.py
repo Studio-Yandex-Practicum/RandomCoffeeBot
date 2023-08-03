1f493eb317569a0c26c76f7304cb63b590588d23
Dima Shelepin <oladushkin777@gmail.com>
2023-08-03 17:41:39 +0300

    Исправлена ошибка асинхронности при ответе бота на сообщение

--- src/bot/plugins.py
+++ src/bot/plugins.py
@@ -4,4 +4,4 @@ from mmpy_bot import Message, Plugin, listen_to
 class MyPlugin(Plugin):
     @listen_to("Привет")
     async def wake_up(self, message: Message):
-        await self.driver.reply_to(message, "Привет, я помогу организовать тебе random coffee")
+        self.driver.reply_to(message, "Привет, я помогу организовать тебе random coffee")
