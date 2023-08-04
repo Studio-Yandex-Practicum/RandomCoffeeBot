3b45e6ca9c149e19ca94684ef0038bc1bbf7bcbf
Dima Shelepin <oladushkin777@gmail.com>
2023-08-04 12:45:11 +0300

    Исправлен конфликт переменных, функция init_bot перенесена

---
+++ src/bot/bot.py
@@ -0,0 +1,19 @@
+from mmpy_bot import Bot, Settings
+
+from bot.plugins import MyPlugin
+from settings import Settings as Settings_bot
+
+
+def init_bot(config: Settings_bot):
+    bot = Bot(
+        settings=Settings(
+            MATTERMOST_URL=config.MATTERMOST_URL,
+            MATTERMOST_PORT=config.MATTERMOST_PORT,
+            MATTERMOST_API_PATH=config.MATTERMOST_API_PATH,
+            BOT_TOKEN=config.BOT_TOKEN,
+            BOT_TEAM=config.BOT_TEAM,
+            SSL_VERIFY=config.SSL_VERIFY,
+        ),
+        plugins=[MyPlugin()],
+    )
+    bot.run()
--- src/settings.py
+++ src/settings.py
@@ -1,9 +1,7 @@
-import logging
-from dataclasses import field
+from logging import DEBUG
 from pathlib import Path
-from typing import Optional, Sequence
 
-from pydantic import DirectoryPath, PostgresDsn
+from pydantic import AnyUrl, DirectoryPath, PostgresDsn
 from pydantic_settings import BaseSettings
 
 ROOT_FOLDER = Path(__file__).parent.parent
@@ -11,32 +9,24 @@ DSN_TEMPLATE = "postgresql+asyncpg://{user}:{password}@{host}:{port}/{db_name}"
 
 
 class Settings(BaseSettings):
-    #    model_config = SettingsConfigDict(
-    #        extra="ignore", case_sensitive=True, env_file=ENV_FILE if (ENV_FILE := ROOT_FOLDER / ".env").exists() else None
-    #    )
+    #        model_config = SettingsConfigDict(
+    #            extra="ignore", case_sensitive=True, env_file=ENV_FILE if (ENV_FILE := ROOT_FOLDER / ".env").exists() else None
+    #        )
     # database connection configuration
-    DB_HOST: str = "localhost"
+    DB_HOST: AnyUrl = "http://localhost"
     DB_PORT: int = 5432
     POSTGRES_DB: str = "postgres"
     POSTGRES_USER: str = "postgres"
     POSTGRES_PASSWORD: str = "postgres"
     # Mattermost bot settings
-    MATTERMOST_URL: str = "localhost"
+    MATTERMOST_URL: str = "http://localhost"
     MATTERMOST_PORT: int = 8065
     MATTERMOST_API_PATH: str = "/api/v4"
     BOT_TOKEN: str = "xzohhrnp5pdiujnizu5x9eqq4r"
     BOT_TEAM: str = "bot"
-    IGNORE_USERS: Sequence[str] = field(default_factory=list)
     SSL_VERIFY: bool = False
-    WEBHOOK_HOST_ENABLED: bool = False
-    SCHEDULER_PERIOD: float = 1.0
-    DEBUG: bool = False
-    LOG_FORMAT: str = "[%(asctime)s][%(name)s][%(levelname)s] %(message)s"
-    LOG_DATE_FORMAT: str = "%m/%d/%Y %H:%M:%S"
-    LOG_FILE: Optional[str] = None
-    SCHEME: str = "http"
     # logging settings
-    LOG_MIN_ERROR_LEVEL: int = logging.DEBUG
+    LOG_MIN_ERROR_LEVEL: int = DEBUG
     LOGGER_NAME: str = "root"
     LOG_ROOT: DirectoryPath = ROOT_FOLDER / "logs"
 
