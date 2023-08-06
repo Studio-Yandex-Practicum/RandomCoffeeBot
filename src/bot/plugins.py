6d93361c4f7001f532d89a5a5d0b37255dd708ac
Gavin Greenhorn <gavingreenhorn@gmail.com>
2023-08-07 00:06:54 +0700

    add registration service

--- src/bot/plugins.py
+++ src/bot/plugins.py
@@ -1,7 +1,16 @@
+import re
+
+from dependency_injector.wiring import Container, Provide, inject
 from mmpy_bot import Message, Plugin, listen_to
 
+from src.bot.services.registration import RegistrationService
 
-class MyPlugin(Plugin):
-    @listen_to("Привет")
-    async def wake_up(self, message: Message):
-        self.driver.reply_to(message, "Привет, я помогу организовать тебе random coffee")
+
+@inject
+class Registration(Plugin):
+    @listen_to("Register", re.IGNORECASE)
+    async def Register(
+        self, message: Message, registration: RegistrationService = Provide[Container.registration_service]
+    ):
+        self.driver.reply_to(message, "Please provide the following information: ...")
+        registration.register("username", "first_name", "last_name")
---
+++ src/bot/services/registration.py
@@ -0,0 +1,16 @@
+from core.db.repository.user import UserRepository
+
+
+class RegistrationService:
+    """Сервис представляющий логику регистрации нового пользователя"""
+
+    def __init__(self, user_repository: UserRepository):
+        self._user_repository = user_repository
+
+    async def register(
+        self,
+        username: str,
+        first_name: str,
+        last_name: str,
+    ):
+        await self._user_repository.create(username, first_name, last_name)
