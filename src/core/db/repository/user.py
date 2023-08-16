96450292743cf46ac080a0c50025b9e532ab78e3
Dima Shelepin <oladushkin777@gmail.com>
2023-08-16 13:44:21 +0300

    Исправлено условие проверки

--- src/core/db/repository/user.py
+++ src/core/db/repository/user.py
@@ -15,9 +15,9 @@ class UserRepository(AbstractRepository[User]):
             instance = await session.scalar(select(self._model).where(self.username == username))
             return instance
 
-    async def create_or_update(self, instance: User) -> User:
+    async def create_or_update(self, instance: User) -> User | None:
         """Создаёт нового или обновляет существующего пользователя в базе данных."""
         db_instance = await self.get_by_username(instance.username)
-        if instance is None:
+        if db_instance is None:
             return await self.create(instance)
         return await self.update(instance.id, db_instance)
