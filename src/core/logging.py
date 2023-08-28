ab4473b32b145d3aa04bd63fdcf77487031f9ecd
Anton A <zantshark@gmailcom>
2023-08-28 10:01:17 +0100

    add color to console logging

--- src/core/logging.py
+++ src/core/logging.py
@@ -1,8 +1,10 @@
 import logging
+import logging.config
 import os
 import sys
 
 import structlog
+from structlog.types import Processor
 
 from src.settings import Settings
 
@@ -12,47 +14,87 @@ def init_logging(settings: Settings) -> None:
     if not os.path.exists(filepath := settings.LOG_ROOT / settings.LOGGER_NAME):
         filepath.touch()
 
-    processors = [
+    processors: list[Processor] = [
         structlog.stdlib.add_log_level,
         structlog.stdlib.add_logger_name,
         structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S"),
         structlog.stdlib.PositionalArgumentsFormatter(),
         structlog.processors.StackInfoRenderer(),
-        structlog.processors.format_exc_info,
-        structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
+        structlog.stdlib.ExtraAdder(),
     ]
 
-    stream_handler = logging.StreamHandler(sys.stdout)
-    stream_handler.setLevel(logging.INFO)
-    stream_handler.setFormatter(
-        structlog.stdlib.ProcessorFormatter(
-            processor=structlog.processors.LogfmtRenderer(),
-            foreign_pre_chain=[
-                structlog.stdlib.add_log_level,
-                structlog.stdlib.add_logger_name,
-                structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S"),
-            ],
-        )
-    )
-    file_handler = logging.FileHandler(filepath, "w")
-    file_handler.setLevel(logging.DEBUG)
-    file_handler.setFormatter(
-        structlog.stdlib.ProcessorFormatter(
-            processor=structlog.processors.JSONRenderer(),
-            foreign_pre_chain=[
-                structlog.stdlib.add_log_level,
-                structlog.stdlib.add_logger_name,
-                structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S"),
-            ],
-        )
-    )
+    logging_dictconfig = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "plain": {
+                "()": structlog.stdlib.ProcessorFormatter,
+                "processors": [
+                    structlog.stdlib.ProcessorFormatter.remove_processors_meta,
+                    structlog.dev.ConsoleRenderer(
+                        colors=False,
+                        exception_formatter=structlog.dev.plain_traceback,
+                    ),
+                ],
+                "foreign_pre_chain": processors,
+            },
+            "colored": {
+                "()": structlog.stdlib.ProcessorFormatter,
+                "processors": [
+                    structlog.stdlib.ProcessorFormatter.remove_processors_meta,
+                    structlog.dev.ConsoleRenderer(colors=True),
+                ],
+                "foreign_pre_chain": processors,
+            },
+        },
+        "handlers": {
+            "default": {
+                "level": settings.LOG_FILE_LEVEL,
+                "class": "logging.StreamHandler",
+                "formatter": "colored",
+            },
+            "file": {
+                "level": settings.LOG_FILE_LEVEL,
+                "class": "logging.handlers.RotatingFileHandler",
+                "filename": os.path.join(settings.LOG_ROOT, settings.LOGGER_NAME),
+                "mode": "a",
+                "maxBytes": settings.LOG_FILE_SIZE,
+                "backupCount": settings.LOG_FILES_TO_KEEP,
+                "encoding": "UTF-8",
+                "formatter": "plain",
+            },
+        },
+        "loggers": {
+            "": {
+                "handlers": ["default", "file"],
+                "level": settings.LOG_FILE_LEVEL,
+                "propagate": True,
+            },
+        },
+    }
 
-    logging.basicConfig(handlers=[stream_handler, file_handler], level=settings.LOG_MIN_ERROR_LEVEL)
+    logging.config.dictConfig(logging_dictconfig)
 
     structlog.configure(
-        processors=processors,
+        processors=processors
+        + [
+            structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
+        ],
         logger_factory=structlog.stdlib.LoggerFactory(),
         wrapper_class=structlog.stdlib.BoundLogger,
-        context_class=dict,
         cache_logger_on_first_use=True,
     )
+
+    root_logger = logging.getLogger()
+
+    def handle_exception(exc_type, exc_value, exc_traceback):
+        if issubclass(exc_type, KeyboardInterrupt):
+            sys.__excepthook__(exc_type, exc_value, exc_traceback)
+            return
+
+        root_logger.error(
+            "An unexpected exception",
+            exc_info=(exc_type, exc_value, exc_traceback),
+        )
+
+    sys.excepthook = handle_exception
